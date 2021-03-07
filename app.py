7053ac55a33381e2cfb3480e9bf1993998c2e584
Emmanuel Alcaraz <alcaraz.emmanuel@gmail.com>
2021-03-06 21:43:59 -0300

    Se agregó la conexión a mysql

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask
 from flask_mongoalchemy import MongoAlchemy
+from flask_sqlalchemy import SQLAlchemy
+import config
 
 app= Flask(__name__)
-app.config["MONGOALCHEMY_DATABASE"]="Prueba"
-db = MongoAlchemy(app)
+app.config.from_object(config)
 
-class Usuarios(db.Document):
-	nombre = db.StringField()
-	mail = db.StringField()
+dbMongo = MongoAlchemy(app)
+dbSQL = SQLAlchemy(app)
+
+class Usuarios(dbMongo.Document):
+	nombre = dbMongo.StringField()
+	mail = dbMongo.StringField()
 
 @app.route("/")
 def Prueba():
 	nuevo = Usuarios(nombre="Emmanuel", mail="alcaraz.emmanuel@gmail.com")
 	nuevo.save()
 
-	usuario = Usuarios.query.filter(Usuarios.nombre == "Emmanuel").first()
+	usuario = Usuarios.query.filter(Usuarios.nombre == "Nadia").first()
 	return usuario.mail
 if __name__ == "__main__":
 	app.run(debug="true")
--- config.py
+++ config.py
@@ -4,5 +4,8 @@ SCRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "mongodb://localhost:27017/prueba"
+MONGOALCHEMY_DATABASE = "Prueba"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SQLALCHEMY_DATABASE_URI = 'mysql://inmunobio:q1w2e3r4@localhost:33060/db_mysql_inmunobio'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
