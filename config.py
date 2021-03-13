921faa0d6676661b77f3cbdc23765044fad7661f
Emmanuel Alcaraz <alcaraz.emmanuel@gmail.com>
2021-03-13 20:54:51 -0300

    Se actualizó el requirement.txt y ya funciona la conexión a mysql y mongo

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ DEBUG = True
 MONGOALCHEMY_DATABASE = "Prueba"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SQLALCHEMY_DATABASE_URI = 'mysql://inmunobio:q1w2e3r4@localhost:33060/db_mysql_inmunobio'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:secret@0.0.0.0:33060/db_mysql_inmunobio'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
