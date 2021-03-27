eff08db9efcffbb42941bc7e4e18b84731d249c3
naye <69222696+Nayesz@users.noreply.github.com>
2021-03-27 20:44:46 -0300

    agregue todos los resources dentro de un mismo archivo

--- api.py
+++ api.py
@@ -1,15 +1,11 @@
 from flask_restful import Resource,Api
-from resources.usuarios import Usuarios
-from resources.usuariosXUsername import UsuarioxUsername
-from resources.permisosXIdUsuario import PermisosXIdUsuario
-from resources.usuariosXIdUsuario import UsuariosXIdUsuario
-from resources.nuevoUsuario import NuevoUsuario
+from resources.usuarios import Usuarios,UsuarioxUsername,PermisosXIdUsuario,UsuariosXIdUsuario,NuevoUsuario
+
 api = Api()
 
 api.add_resource(Usuarios, '/api/usuarios')
 api.add_resource(UsuarioxUsername, '/api/usuario/<string:username>')
 api.add_resource(PermisosXIdUsuario, '/api/permisos/<int:id>')
-api.add_resource(UsuariosXIdUsuario, '/api/usuario/get/<int:id>')
+api.add_resource(UsuariosXIdUsuario, '/api/usuario/<int:id>')
 api.add_resource(NuevoUsuario, '/api/usuario/put')
 
-#aca le pedimos la id para crear?? -> ver como separar las rutas o mejorar esto
--- resources/usuarios.py
+++ resources/usuarios.py
@@ -1,6 +1,7 @@
 from models.usuario import Usuario
 from flask_restful import Resource,Api
-
+from flask_jwt import jwt_required
+from flask import jsonify, request
 class Usuarios(Resource):
 
     def get(self):
@@ -9,3 +10,86 @@ class Usuarios(Resource):
         if usuarios:
             return [usuario.json() for usuario in usuarios]
         return {'name': 'None'},404
+
+class UsuariosXIdUsuario(Resource):
+
+    @jwt_required()
+    def get(self, id):
+        usuario = Usuario.find_by_id(id)
+        if usuario:
+            return usuario.json()
+        return {'name': 'None'},404
+    
+    @jwt_required()
+    def post(self,id):
+
+        datos = request.get_json(silent=True)
+        if (datos):
+            nombre = datos['nombre']
+            username = datos['username']
+            password = datos['password']
+            mail = datos['mail']
+            direccion = datos['direccion']
+            telefono = datos['telefono']
+            usuario = Usuario(nombre , username , mail,password,direccion ,telefono )
+            db.session.add(usuario)
+            db.session.commit()
+            return {'Status':'ok'}
+        return {'name': 'None'},404
+    @jwt_required()
+    def put(self,id):
+        modificaciones = request.get_json()
+        #aca los parametros pueden ser direccion y/o telefono y/o mail y/o contraseña
+        usuario = Usuario.find_by_id(id)
+        if usuario:
+            for clave,valor in modificaciones.items():
+                if hasattr(usuario, clave):
+                    setattr(usuario, clave, valor)
+            db.session.commit()
+            #no esta contemplando que esa clave exista o no->solucionado con hasattr pero meh
+            return {'Status':'ok'}
+        return {'name': 'None'},404
+    
+class PermisosXIdUsuario(Resource):
+	@jwt_required()
+	def get(self, id):
+		#no hice la busqueda por username, me parecio mejor hacerla por id ya que es unico ->VER
+		usuario = Usuario.find_by_id(id)
+		permisos = {}
+		if usuario:
+		
+			for permiso in usuario.id_permisos:
+				permisos[permiso.id] = permiso.descripcion      
+			return jsonify(permisos)
+				
+		return  {'name': 'None'},404
+
+
+class NuevoUsuario(Resource):
+    
+    @jwt_required()
+    def post(self):
+        datos = request.get_json(silent=True)
+        if (datos):
+            nombre = datos['nombre']
+            username = datos['username']
+            password = datos['password']
+            mail = datos['mail']
+            direccion = datos['direccion']
+            telefono = datos['telefono']
+            usuario = Usuario(nombre , username , mail,password,direccion ,telefono )
+            db.session.add(usuario)
+            db.session.commit()
+            return {'Status':'ok'},200
+        return {'name': 'None'},404
+   
+    
+class UsuarioxUsername(Resource):
+	@jwt_required()
+	def get(self, username):
+		#usuario = Usuario.query.filter_by(username = username).first()
+		usuario = Usuario.find_by_username(username)
+		if usuario:
+			return usuario.json()
+		return {'name': 'None'},404
+
