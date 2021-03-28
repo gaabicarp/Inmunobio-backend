ad4a049b841917116da73588ee2e67f9b0998d16
naye <69222696+Nayesz@users.noreply.github.com>
2021-03-28 01:54:38 -0300

    cambio de ruta para nuevo usuario

--- api.py
+++ api.py
@@ -6,5 +6,5 @@ api = Api()
 api.add_resource(Usuarios, '/api/usuarios')
 api.add_resource(UsuarioxUsername, '/api/usuario/<string:username>')
 api.add_resource(UsuariosXIdUsuario, '/api/usuario/<int:id>')
-api.add_resource(NuevoUsuario, '/api/usuario/put')
+api.add_resource(NuevoUsuario, '/api/nuevoUsuario/')
 
