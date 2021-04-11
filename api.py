0d696975cbdbdf5e3394b276649effc6be63d9b5
Emmanuel Alcaraz <alcaraz.emmanuel@gmail.com>
2021-04-11 05:14:39 -0300

    Se crearon esquemas de experimento, endpoints y consultas

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
-from flask_restful import Resource,Api
+from flask_restful import Api
 from resources.usuarios import Usuarios,UsuarioxUsername,UsuariosXIdUsuario,NuevoUsuario, ActualizarPermisos,ObtenerUsuariosParaProyecto
 from resources.proyecto import *
-
+from resources.experimento import ExperimentoResource, Experimentos
 api = Api()
 
 api.add_resource(Usuarios, '/api/usuarios')
@@ -15,3 +15,8 @@ api.add_resource(NuevoProyecto, '/api/nuevoProyecto')
 api.add_resource(ProyectoID, '/api/proyectoID')
 api.add_resource(CerrarProyecto, '/api/cerrarProyecto')
 api.add_resource(ModificarProyecto, '/api/modificarProyecto')
+
+api.add_resource(Experimentos, '/api/proyecto/<int:idProyecto>/experimentos')
+api.add_resource(ExperimentoResource, '/api/experimento/<int:idExperimiento>', endpoint='experimento')
+api.add_resource(ExperimentoResource, '/api/nuevoExperimento', endpoint='nuevo_experimento')
+api.add_resource(ExperimentoResource, '/api/cerrarExperimento/<int:idExperimento>', endpoint='cerrar_experimento')
