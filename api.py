2f0eabb482a125abd03f0800a86daa5a58b2b9fb
Nayesz <69222696+Nayesz@users.noreply.github.com>
2021-04-13 15:28:40 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,10 @@
 from flask_restful import Api
 from resources.usuarios import Usuarios,UsuarioxUsername,UsuariosXIdUsuario,NuevoUsuario, ActualizarPermisos,ObtenerUsuariosParaProyecto
 from resources.proyecto import *
-<<<<<<< HEAD
+
 from resources.grupoDeTrabajo import NuevoGrupoDeTrabajo,GrupoDeTrabajoPorId
-=======
+
 from resources.experimento import ExperimentoResource, Experimentos
->>>>>>> 0d696975cbdbdf5e3394b276649effc6be63d9b5
 api = Api()
 
 api.add_resource(Usuarios, '/api/usuarios')
