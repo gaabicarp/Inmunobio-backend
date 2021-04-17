f63d0e3b0d59688630eb76b1e2a2bebbc07376e0
naye <69222696+Nayesz@users.noreply.github.com>
2021-04-17 00:16:02 -0300

    cambio en la estructura de grupo de trabajo, se agrego servicios

--- resources/grupoDeTrabajo.py
+++ resources/grupoDeTrabajo.py
@@ -3,51 +3,43 @@ from marshmallow import Schema, fields, post_load, ValidationError
 from bson import ObjectId
 from dateutil import parser
 from flask import jsonify
-from models.mysql.usuario import Usuario
 import json
 from models.mongo.grupoDeTrabajo import GrupoDeTrabajoSchema,GrupoDeTrabajo
 from flask_restful import Resource,Api
 from flask import jsonify, request
-
-
+from servicios.GrupoDeTrabajoService import GrupoDeTrabajoService
  
 class NuevoGrupoDeTrabajo(Resource):
         def post(self):
             datos = request.get_json()
             if datos:
-                schemaGrupo = GrupoDeTrabajoSchema()
                 try:
-                    grupoCreado = schemaGrupo.load(datos)
+                    grupoCreado = GrupoDeTrabajoSchema().load(datos)
                     grupoCreado.save()
                     return {'Status':'ok'},200
                 except ValidationError as err:
                     return {'error': err.messages},404
             return {'name': datos},404
 
+class MiembrosGrupoDeTrabajo(Resource):
+    def put(self,id):
+            #edita miembros de un grupo de trabajo, esta accion solo puede realizarla el jefe del grupo.
+            #recibe id por parametro y miembros por json.
+            # to-do: ver si es necesario pasar todo x json
+            datos = request.get_json()
+            if datos:
+                grupoAModif = GrupoDeTrabajoService.find_by_id(id)
+                if (grupoAModif):
+                        GrupoDeTrabajoService.modificarMiembros(id,datos,grupoAModif)
+                        return {'Status':'ok'},200   
+                return  {'error':'El grupo de trabajo no existe'},404
+            return {'name': datos},404
 
 class GrupoDeTrabajoPorId(Resource):
-    def put(self,id):
-        #edita miembros o jefe de grupo
-        datos = request.get_json()
-        if datos:
-            if('id' in datos ):
-                grupoAModificar = GrupoDeTrabajo.find_by_id(datos['id'])
-                if (grupoAModificar):
-                    GrupoDeTrabajo.modificarMiembros(datos)
-                    return {'Status':'ok'},200   
-                return  {'error':'El grupo de trabajo no existe'},404
-            return {'error':'No se envio la id'},404
-        return {'name': datos},404
-
+   
     def get(self,id):
-        #obtiene grupo de trabajo mediante id
-        #datos = request.get_json()
-        #if datos:
-        #if('id' in datos ):
-        grupoConsulta= GrupoDeTrabajo().find_by_id(id)
+        #dado un id de grupo de trabajo devuelve todos los datos del grupo de GrupoDeTrabajo si lo encuentra
+        grupoConsulta= GrupoDeTrabajoService.find_by_id(id)
         if (grupoConsulta):
-                return grupoConsulta.json()
-        return  {'error':'El grupo de trabajo no existe'},404
-                
-        #return {'error':'No se envio la id'},404
-        return {'name': datos},404
+            return GrupoDeTrabajoService.json(grupoConsulta)
+        return  {'error':'El grupo de trabajo no existe'},404    
