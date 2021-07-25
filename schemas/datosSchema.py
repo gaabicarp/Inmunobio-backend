3ca197b4968c96112916d636269214bf26672bd2
nayesz <69222696+Nayesz@users.noreply.github.com>
2021-07-24 23:24:15 -0300

    Schema de prueba

---
+++ schemas/datosSchema.py
@@ -0,0 +1,9 @@
+from marshmallow import Schema, fields
+from .usuarioSchema import UsuarioSchema
+from .proyectoSchema import ProyectoSchema
+
+class DatosSchema(Schema):
+    usuario = fields.Nested(UsuarioSchema,many=True)
+    proyecto = fields.Nested(ProyectoSchema,many=True)
+
+    
