3732dca1318f6248a1167fa74d11502cfd759856
Noh <nadianohely@gmail.com>
2021-07-25 02:00:12 -0300

    actualizacion config

--- config.py
+++ config.py
@@ -3,11 +3,13 @@ import os
 SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 PWD = os.path.abspath(os.curdir)
 
+
+
 DEBUG = True
 MONGODB_DB = 'db_mongo_inmunobio'
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'mysql://root:secret@0.0.0.0:33060/db_mysql_inmunobio'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:secret@mysql:3306/db_mysql_inmunobio'
 UPLOAD_FOLDER = PWD + '/uploads/'
 
