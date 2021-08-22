af5c08d791570fb7126a83447d4bc7367ba11d0a
nayesz <69222696+Nayesz@users.noreply.github.com>
2021-08-21 21:41:23 -0300

    cambios en app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from servicios.usuarioService import UsuarioService
 app= Flask(__name__)
 app.config.from_object(config)
 
-#app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 ############################db configuracion
 db.init_app(app)
 
@@ -29,26 +28,18 @@ nojwt = JWT(app, authenticate, identity)
 CORS(app)
 cors = CORS(app,resources={r"/api/*":{"origins":"*"}})
 
-
 ############################ Api configuracion
 from api import api
 api.init_app(app)
-
 ############################
 app.config['JSON_SORT_KEYS'] = False
 
-from flask import send_from_directory
-@app.route("/uploads/<path:path>")
-def static_dir(path):
-    return send_from_directory("uploads", path)
-	
 if __name__ == "__main__":
 	if app.config['DEBUG']:
 		@app.before_first_request
 		def create_tables():
 			db.create_all()
 
-	
 	app.run(port=8080 ,debug=True)	
 
 """ if __name__ == "__main__":
