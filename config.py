33e1c3efa9c0b0b2737396195452cbe29ec3d3fc
nayesz <69222696+Nayesz@users.noreply.github.com>
2021-08-23 16:49:54 -0300

    modifico config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
 MONGODB_DB = 'db_mongo_inmunobio'
-MONGODB_HOST = 'localhost'
+MONGODB_HOST = 'mongo'
 #cambiar a mongo
 MONGODB_PORT = 27017
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'mysql://root:secret@0.0.0.0:33060/db_mysql_inmunobio'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:secret@mysql:33060/db_mysql_inmunobio'
 #cambiar a mysql:33060
 UPLOAD_FOLDER = PWD + '/uploads/'
 
